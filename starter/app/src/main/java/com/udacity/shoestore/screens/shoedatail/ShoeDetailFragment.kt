9cc66f9e6d7675a432ed0d0a931397eb165ef041
Margaret Lin <margaret.lin@soundcloud.com>
2020-12-18 13:23:15 +0800

    Add shoe detail screen

---
+++ starter/app/src/main/java/com/udacity/shoestore/screens/shoedatail/ShoeDetailFragment.kt
@@ -0,0 +1,20 @@
+package com.udacity.shoestore.screens.shoedatail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.R
+
+class ShoeDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_shoe_detail, container, false)
+    }
+
+}
